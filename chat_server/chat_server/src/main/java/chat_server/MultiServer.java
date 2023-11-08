00e1c23f559ef4cb001179eec7e7002290fdc4fe
lai2-24 <lai2-24@itismeucci.com>
2023-11-08 08:39:45 +0100

    First commit

---
+++ chat_server/chat_server/src/main/java/chat_server/MultiServer.java
@@ -0,0 +1,13 @@
+package chat_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class MultiServer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
